ddbf934e190852512b40b3d2fecf3db81ac7b04e
Pierre Östlund <Pierre.Ostlund@alten.se>
2016-04-04 20:29:29 +0200

    android: Add dummy place holder for the serial driver

---
+++ android/serial/src/main/java/se/alten/swearc/comm/SampleFile.java
@@ -0,0 +1,10 @@
+package se.alten.swearc.comm;
+
+public class SampleFile {
+
+  // TODO: Remove this file and implement the serial driver here
+  public static int add(int a, int b) {
+    return a + b;
+  }
+
+}
